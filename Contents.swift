fcc08b9dd630b44d8ee6f97ce50b3e2e1475e5eb
Asif Mimi <30404856+asifmimi1@users.noreply.github.com>
2018-05-18 21:19:07 +0600

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,52 @@
+var name = "asif"
+
+var nickName = "Mimi"
+name
+nickName
+var shortNmane = "Rabbi"
+
+var integer : Float = 4.9
+
+let numbner = 25
+numbner
+//number =52 /*wrong; one's you delared let the value is fixed*/
+let fNumber:Float = 3.1416
+
+let myCountry = "Banngladesh"
+let Bangldesh = true
+true
+false
+
+1 / 2
+var a : Double = 3
+//var b : Double = 5
+a *= 5
+var myName = "Asif"
+
+print("The name is \(myName)")
+
+
+
+//var x = "Hello,World"
+//print("the first program \(myCountry)")
+
+var y =  0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
